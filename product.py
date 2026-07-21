746e8f720c64485ec90f42ec1b3745c1ddbe4c74
misamkacen88-Tkach <misamkacen88@gmail.com>
2026-07-21 08:57:48 +0000

    Добавил продукт

---
+++ product.py
@@ -0,0 +1,8 @@
+class Product:
+
+    def __init__(self, id, name, calories, protein, fat, cards, vitamin, minerals):
+
+        self.id = id
+        self.name = name
+        
+        self.calories = calories
